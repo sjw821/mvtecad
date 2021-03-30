395eeedafc8cba603de14bac60428e90634eeb0e
jaewoong <sjw821@gmail.com>
2021-03-30 15:50:20 +0900

    first commit

---
+++ get_data.py
@@ -0,0 +1,18 @@
+import tarfile
+import tqdm
+import urllib.request
+import sys 
+
+current_dir = '/home/ubuntu/mvtecad/'
+data_dir = current_dir+'dataset/'
+
+# def _progress(count, block_size, total_size):
+#     sys.stdout.write('\rDownloading %.2f%%' % (float(count * block_size) / float(total_size) * 100.0))
+#     sys.stdout.flush()
+
+# #download tar.xz file
+# url = "ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_anomaly_detection.tar.xz"
+# urllib.request.urlretrieve(url, filename=data_dir+'mvtecad.tar.xz', reporthook=_progress)
+# #unzip
+with tarfile.open(data_dir+'mvtecad.tar.xz', 'r:xz') as tar:
+    tar.extractall(path=data_dir)
