22bd7ea05155f8654c8a426203e29c957fdbae07
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-12-16 14:50:35 +0000

    Adding tarfile member sanitization to extractall()

--- get_data.py
+++ get_data.py
@@ -15,4 +15,26 @@ url = "ftp://guest:GU.205dldo@ftp.softronics.ch/mvtec_anomaly_detection/mvtec_an
 urllib.request.urlretrieve(url, filename=data_dir+'mvtecad.tar.xz', reporthook=_progress)
 #unzip
 with tarfile.open(data_dir+'mvtecad.tar.xz', 'r:xz') as tar:
-    tar.extractall(path=data_dir)
+    
+    import os
+    
+    def is_within_directory(directory, target):
+        
+        abs_directory = os.path.abspath(directory)
+        abs_target = os.path.abspath(target)
+    
+        prefix = os.path.commonprefix([abs_directory, abs_target])
+        
+        return prefix == abs_directory
+    
+    def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+    
+        for member in tar.getmembers():
+            member_path = os.path.join(path, member.name)
+            if not is_within_directory(path, member_path):
+                raise Exception("Attempted Path Traversal in Tar File")
+    
+        tar.extractall(path, members, numeric_owner=numeric_owner) 
+        
+    
+    safe_extract(tar, path=data_dir)
